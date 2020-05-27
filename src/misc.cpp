8f0f115747f778cd2f0d4a934a7efb10275b18ff
Mateusz Zaleski <mateusz.a.zaleski@gmail.com>
2020-05-27 12:21:20 +0200

    Added prime searching algorithm
        As the title says + small header refactor
        for better visibility

--- src/misc.cpp
+++ src/misc.cpp
@@ -3,6 +3,27 @@
 
 namespace mz {
 
+std::vector<int> eratosthenes_sieve(size_t limit)
+{
+    std::vector<bool> primes(limit, true);
+
+    int firstPrime = 2;
+
+    for (int i = 0, j = firstPrime; i < limit; ++i, ++j) {
+        if (primes[i]) {
+            for (int k = j + i; k < limit; k += j)
+                primes[k] = false;
+        }
+    }
+
+    std::vector<int> result;
+    for (int i = 0; i < primes.size(); ++i)
+        if (primes[i])
+            result.push_back(firstPrime + i);
+
+    return result;
+}
+
 int greatest_common_divisor(int a, int b)
 {
     if (a != 0) {
